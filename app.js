1fd5972085c2990f918a5e28d4d43aeaaf0016a2
Zachary Smith <billyzacsmith@gmail.com>
2016-03-14 21:37:05 -0600

    Put personality functionality in a module.

--- app.js
+++ app.js
@@ -1,39 +1,9 @@
-var watson = require('watson-developer-cloud')
-require('dotenv').config()
+var personality = require('./personality')
 
-var personality_insights = watson.personality_insights({
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  version: 'v2'
-})
-
-var textToAnalyze =
-['WHEN lilacs last in the door-yard bloom’d, ',
-'And the great star early droop’d in the western sky in the night, ',
-'I mourn’d—and yet shall mourn with ever-returning spring. ',
-
-'O ever-returning spring! trinity sure to me you bring; ',
-'Lilac blooming perennial, and drooping star in the west,',
-'And thought of him I love. ',
-
-'O powerful, western, fallen star! ',
-'O shades of night! O moody, tearful night! ',
-'O great star disappear’d! O the black murk that hides the star! ',
-'O cruel hands that hold me powerless! O helpless soul of me!',
-'O harsh surrounding cloud, that will not free my soul!',
-'In the door-yard fronting an old farm-house, near the white-wash’d palings, ',
-'Stands the lilac bush, tall-growing, with heart-shaped leaves of rich green, ',
-'With many a pointed blossom, rising, delicate, with the perfume strong I love, ',
-'With every leaf a miracle......and from this bush in the door-yard,',
-'With delicate-color’d blossoms, and heart-shaped leaves of rich green, ',
-'A sprig, with its flower, I break.'
-].join()
-
-personality_insights.profile({
-  text: textToAnalyze },
-  function (err, response) {
-    if (err)
-      console.log('error:', err)
-    else
-      console.log(JSON.stringify(response, null, 2))
-})
+personality('@billyzacsmith')
+  .then(function(personalityProfile) {
+    console.log(personalityProfile.traits)
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
---
+++ personality.js
@@ -0,0 +1,65 @@
+module.exports = personality
+
+function personality(username) {
+  return new Promise(function(resolve, reject) {
+    var Twit = require('twit')
+    var watson = require('watson-developer-cloud')
+
+    require('dotenv').config()
+
+    var T = new Twit({
+      consumer_key: process.env.CONSUMER_KEY,
+      consumer_secret: process.env.CONSUMER_SECRET,
+      access_token: process.env.ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+      timeout_ms: 60*1000
+    })
+
+    var personality_insights = watson.personality_insights({
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      version: 'v2'
+    })
+
+    var tpath = getUserHandle('@billyzacsmith').tpath
+    var options = getUserHandle('@billyzacsmith').options
+
+    T.get(tpath, options, function(error, data) {
+      if (error) return reject(error)
+      var tweets = []
+      for (var i = 0; i < data.length; i++) {
+        tweets.push(data[i].text);
+      }
+
+      var textToAnalyze = tweets.join(' ');
+
+      personality_insights.profile({
+        text: textToAnalyze },
+        function (err, response) {
+          if (error) return reject(error)
+          else
+            // console.log((response.tree.children[1].children[0].children));
+            var personalityProfile = {
+              traits: response.tree.children[0].children[0].children,
+              needs: response.tree.children[1].children[0].children
+            }
+            resolve (personalityProfile)
+      })
+    })
+  })
+}
+
+///////////
+function getUserHandle(query) {
+  var options = {
+    screen_name: query,
+    count: 200,
+    include_rts: false
+  }
+  var tpath = 'statuses/user_timeline'
+
+  return {
+    options: options,
+    tpath: tpath
+  }
+}
